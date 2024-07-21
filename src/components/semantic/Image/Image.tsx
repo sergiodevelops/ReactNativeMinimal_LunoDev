a02a44e1b0340c32c260105b3cd365fc91944aea
Ch3k0 <sergioarieljuarez@gmail.com>
2024-07-21 08:39:22 +0200

    ADD:add image download minimal protection with right click when is web on mobile

--- src/components/semantic/Image/Image.tsx
+++ src/components/semantic/Image/Image.tsx
@@ -1,29 +1,35 @@
-import React, {ReactElement} from "react";
-import {Image as Img, ImageProps, Pressable, PressableProps, View} from "react-native";
+import React, {HTMLAttributes, ReactElement} from "react";
+import {Image as Img, ImageProps, ImageStyle, Pressable, PressableProps} from "react-native";
 import MainView__image from "../../../styles/ts/MainView/__image/MainView__image";
+import Division from "../Division/Division";
 
 
 type ImgProps = {
     onPointerEnter(): void,
     onPointerLeave(): void,
-} & ImageProps & PressableProps;
+} & HTMLAttributes<HTMLImageElement> & ImageProps & PressableProps;
 export default function Image(props: ImgProps): ReactElement {
 
     const {mainView__image} = MainView__image();
 
 
     return (
-        <View>
+        <Division
+            className={'img-container'}
+            {...props}
+            style={props.style as HTMLAttributes<HTMLDivElement>}
+        >
             <Pressable {...props as PressableProps}>
                 <Img
                     {...props as ImageProps}
                     style={[
                         mainView__image,
+                        {pointerEvents: 'none'} as ImageStyle,
                         props.style,
                     ]}
                     role={'img'}
                 />
             </Pressable>
-        </View>
+        </Division>
     );
 }
