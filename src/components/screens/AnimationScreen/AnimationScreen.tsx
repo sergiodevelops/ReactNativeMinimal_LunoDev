2323978df7013253dc8ac7e5e88cf0aeed2e3e42
Ch3k0 <sergioarieljuarez@gmail.com>
2024-07-21 08:42:48 +0200

    MOD:simplify screen TODO animations

--- src/components/screens/AnimationScreen/AnimationScreen.tsx
+++ src/components/screens/AnimationScreen/AnimationScreen.tsx
@@ -1,129 +1,34 @@
-import React, {useEffect, useId, useRef} from "react";
+import React, {useEffect, useId} from "react";
 import MainView from "../../../styles/ts/MainView/MainView";
-import useStyleThemeStore from "../../../hooks/useStyleThemeStore";
-import Button from "../../semantic/Button/Button";
 import {NavigationProps} from "../../../app/App";
-import useRandomColor from "../../../hooks/useRandomColor";
-import Heading from "../../semantic/Heading/Heading";
-import FlexResponsive from "../../FlexResponsive/FlexResponsive";
-import {RandomColorProp} from "../../../constants/types";
-import useMilisecondsInterval from "../../../hooks/useMilisecondsInterval";
 import ScreenWrapper from "../ScreenWrapper/ScreenWrapper";
 import useRefCountRenders from "../../../hooks/useRefCountRenders";
-import {Animated} from "react-native";
-import MainView__animation_fadeOpacity, {
-    fadeInOpacity
-} from "../../../styles/ts/MainView/__animation/MainView__animation_fadeOpacity";
-import Paragraph from "../../semantic/Paragraph/Paragraph";
+import {Text} from "react-native";
 
 
+// TODO
 export default function AnimationScreen(props: NavigationProps) {
-    // fadeAnim will be used as the value for "opacity". Initial Value: 0
-    const fadeAnim = useRef(new Animated.Value(0)).current;
-    // fadeAnim will be used as the value for "opacity". Initial defaultValue: 1
-    const {fadeOpacity} = MainView__animation_fadeOpacity(fadeAnim);
 
     const {navigation} = props;
     const id = `MySemantic-${useId()}`;
     const {count} = useRefCountRenders();
 
-    const {mainView_container, mainView} = MainView();
-    const {toogleStyleTheme, currentIconStyleTheme} = useStyleThemeStore();
     const {
-        randomColor,
-        resetRandomColor,
-        changeRandomColor,
-    } = useRandomColor(mainView_container.backgroundColor as RandomColorProp);
+        mainView_container,
+        mainView,
+    } = MainView();
+
     const handleOnPressButtonGoToHomeScreen = () => navigation.navigate("home");
-    const {start, puase, reset} = useMilisecondsInterval({
-        intervalCallback: changeRandomColor,
-        resetCallback: resetRandomColor,
-        interval: 500,
-    })
+
 
 
     useEffect(() => {
-        !!fadeAnim && fadeInOpacity({fadeAnim});
-    }, [fadeAnim]);
+    }, []);
 
 
     return (
-        <ScreenWrapper
-            style={mainView_container}
-            animatedStyle={fadeOpacity}
-        >
-            <FlexResponsive style={[
-                mainView,
-                {
-                    flex: 1,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    backgroundColor: randomColor || mainView_container.backgroundColor,
-                },
-            ]}>
-
-                <FlexResponsive container>
-                    <Paragraph
-                        children={`Number of Renders: ${String(count)}`}
-                    />
-
-                    <FlexResponsive item>
-                        <Heading variant={'h1'}>🤩 DOM manipulation in React 🤝</Heading>
-                    </FlexResponsive>
-                    <FlexResponsive item>
-                        <Paragraph children={`Current random color is: ${randomColor}`}/>
-                    </FlexResponsive>
-
-                    <FlexResponsive item
-                                    xs={{part: 10}}
-                                    md={{part: 7}}
-                    >
-                        <Button type={'accept'} onPress={start}>🟢 START</Button>
-                    </FlexResponsive>
-                    <FlexResponsive item
-                                    xs={{part: 10}}
-                                    md={{part: 7}}
-                    >
-                        <Button type={'default'} onPress={puase}>🟡 PAUSE</Button>
-                    </FlexResponsive>
-                    <FlexResponsive item
-                                    xs={{part: 10}}
-                                    md={{part: 7}}
-                    >
-                        <Button type={'decline'} onPress={reset}>🛑 RESET</Button>
-                    </FlexResponsive>
-
-                </FlexResponsive>
-
-                <FlexResponsive item
-                                xs={{part: 10}}
-                                md={{part: 7}}
-                >
-                    {/* BUTTON default */}
-                    <Button
-                        id={`${id}-button-default`}
-                        type={'accept'}
-                        onPress={toogleStyleTheme}
-                        children={`Toogle Theme "default" Button ${currentIconStyleTheme}`}
-                    />
-                </FlexResponsive>
-
-                <FlexResponsive item
-                                xs={{part: 10}}
-                                md={{part: 7}}
-                >
-                    {/* BUTTON accept */}
-                    <Button
-                        id={`${id}-button-goToDetailsScreen`}
-                        type={"default"}
-                        onPress={handleOnPressButtonGoToHomeScreen}
-                        children={`👈 Go back to "Home" screen`}
-                    />
-                </FlexResponsive>
-
-
-
-            </FlexResponsive>
+        <ScreenWrapper style={mainView_container}>
+            <Text>AnimationScreen</Text>
         </ScreenWrapper>
     );
 }
